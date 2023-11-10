f9b136064d743ed711de3c85f81e4342b4be04c3
yondikavl <yondikaviolanda@gmail.com>
2023-11-10 11:42:55 +0700

    fix(contact). add text

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
 
 export const metadata = {
   title: "Contact | Yondika",
 };
 
 const Contact = () => {
-  return <div className="pt-96">Ini Contact</div>;
+  return (
+    <div className="py-48 text-center text-3xl font-bold text-white flex flex-col items-center gap-4">
+      Connect with me
+      <ArrowDropDownCircleIcon />
+    </div>
+  );
 };
 
 export default Contact;
